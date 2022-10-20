59e350936c1df98e7557570f6d4c1887eb242a16
gabrielsalinas24 <113557530+gabrielsalinas24@users.noreply.github.com>
2022-10-20 14:19:18 -0700

    Add files via upload

--- buzzfuzz.java
+++ buzzfuzz.java
@@ -1,23 +1,30 @@
 public class buzzfuzz{
-public void run() {
-    int i=1;
-    int fizz=3;
-    int buzz=5;
-    for (i= 1; i < 101; i++){
-        if (i == fizz){
-        fizz+=3;
-        System.out.println("fuzz");
-    } else System.out.println(i);
-        if (i == buzz) {
-        buzz+=5;
-        System.out.println("buzz");
+    public void run() {
+        int i=1;
+        int fizz=3;
+        int buzz=5;
+        int fizzbuzz=15;
+        for (i= 1; i < 100; i++){
+            if (i == fizzbuzz){
+                buzz+=5;
+                fizz+=3;
+                fizzbuzz+=15;
+                System.out.println("fizzbuzz");
+            }
+            else if (i == buzz) {
+                buzz+=5;
+                System.out.println("buzz");
+            } else if (i == fizz){
+                fizz+=3;
+                System.out.println("fizz");
+            } else System.out.println(i);
+        }
+
     }
-}
-}
-public static void main(String[] args){ 
-    buzzfuzz hw = new buzzfuzz();
-    hw.run();
+
+    public static void main(String[] args){ 
+        buzzfuzz hw = new buzzfuzz();
+        hw.run();
     } 
 }   
 
-    
